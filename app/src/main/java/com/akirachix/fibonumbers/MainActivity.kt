f5fd28085ef7e619ce2eb6e5ab7d4450e5258188
Scarlet Alkeyo <scarletalkeyo@gmail.com>
2024-06-18 17:31:09 +0300

    made changes

--- app/src/main/java/com/akirachix/fibonumbers/MainActivity.kt
+++ app/src/main/java/com/akirachix/fibonumbers/MainActivity.kt
@@ -8,20 +8,28 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.akirachix.fibonumbers.databinding.ActivityMainBinding
-
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val numbers = fibonacci(100)
+        binding.rvNumbers.layoutManager = LinearLayoutManager(this)
+        val numbersAdapter = FibonacciAdapter(numbers)
+        binding.rvNumbers.adapter =numbersAdapter
 
-        val numbers = listOf(1,1,2,3,5,8,13,21,34,55, 89,)
+    }
 
-        binding.rvNumbers.layoutManager= LinearLayoutManager(this)
-        val  namesAdapter =NumbersRecyclerViewAdapter(numbers)
-        binding.rvNumbers.adapter = numbers.Adapter
+//    private fun FibonacciAdapter(numbers: List<Int>): RecyclerView.Adapter<RecyclerView.ViewHolder>? {
+//
+//    }
 
-
+    fun fibonacci(n: Int): List<Int> {
+        val numbers = mutableListOf(0, 1)
+        while (numbers.size < n) {
+            numbers.add(numbers[numbers.lastIndex] + numbers[numbers.lastIndex - 1])
+        }
+        return numbers
     }
 }
